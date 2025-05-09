6fdf670da9d392181e56bf40f032af6efc98b320
helenren666 <rhongqiao@gmail.com>
2025-05-09 14:43:49 -0400

    hiii

--- Sophomore_Sem2_Project5/player1.js
+++ Sophomore_Sem2_Project5/player1.js
@@ -1,8 +1,9 @@
 // player1.js
 document.getElementById("searchBtn").addEventListener("click", () => {
+    console.log('user clicked123')
     const artist = document.getElementById("artistInput").value;
     if (artist) {
-      window.location.href = `/Sophomore_Sem2_Project5/player2.html?artist=${encodeURIComponent(artist)}`;
+      window.location.href = `player2.html?artist=${encodeURIComponent(artist)}`;
     }
   });
   
